43cb0bef2f995861e4b56560c7365f8f809b9ee5
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-06 14:59:19 +0200

    update js

--- curriculum-projects(w.-tweaks)/Budget-Tracker/budgetTracker.js
+++ curriculum-projects(w.-tweaks)/Budget-Tracker/budgetTracker.js
@@ -6,3 +6,13 @@ const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 // query for the add entry button (el.button)
 const addEntryButton = document.getElementById('add-entry');
+// query for the clear button (el.button)
+const clearButton = document.getElementById('clear');
+// query for the output (el.div)
+const output = document.getElementById('output');
+// declare a reassignable variable with the boolean value false
+let isError = false;
+
+// since user input can be unformatted and unfit for processing data, it needs 'cleaning'
+// all user inputs are taken as strings.
+function cleanInputString
