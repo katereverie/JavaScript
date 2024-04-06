a535dc3eb21c10d068083c77b55ac2fc4976c87f
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-06 14:50:29 +0200

    update js

---
+++ curriculum-projects(w.-tweaks)/Budget-Tracker/budgetTracker.js
@@ -0,0 +1,8 @@
+// query for the whole form of budget tracker (el.form)
+const budgetCounter = document.getElementById('budgetTracker');
+// query for the set budget number (el.input)
+const budgetNumberInput = document.getElementById('budget');
+// query for the entry-dropdown menu (el.select)
+const entryDropdown = document.getElementById('entry-dropdown');
+// query for the add entry button (el.button)
+const addEntryButton = document.getElementById('add-entry');
