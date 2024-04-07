3421011334906bd9b6424b0717592c25216d1837
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-07 11:56:55 +0200

    update html, js

--- curriculum-projects(w.-tweaks)/Budget-Tracker/budgetTracker.js
+++ curriculum-projects(w.-tweaks)/Budget-Tracker/budgetTracker.js
@@ -1,6 +1,6 @@
 // .getElementById targets directly HTML elements
 // query for the whole form of budget tracker (el.form)
-const budgetCounter = document.getElementById('budgetTracker');
+const budgetCounter = document.getElementById('budget-Tracker');
 // query for the set budget number (el.input)
 const budgetNumberInput = document.getElementById('budget');
 // query for the entry-dropdown menu (el.select)
@@ -22,7 +22,14 @@ function cleanInputString(str) {
   return str.replace(regex, '');
 }
 
-// 
+// this function checks if user inputs are valid
+function isInvalidInput(str) {
+  // look for numbers using scientific notation
+  // I doubt this is common; but here we go.
+  const regex = /\d+e\d+/i;
+  // return matched inputs and null if no match is found. 
+  return str.match(regex);
+}
 
 
 
@@ -60,20 +67,80 @@ function calculateSums(e) {
 
   // get the entry inputs
   const clothingSums = getSumsFromInputs(clothingNumberInputs);
-  const foodSums = getSumsFromInputs(clothingNumberInputs);
-  const livingSums = getSumsFromInputs(clothingNumberInputs);
-  const transportSums = getSumsFromInputs(clothingNumberInputs);
-  const entertainmentSums = getSumsFromInputs(clothingNumberInputs);
-  const othersSums = getSumsFromInputs(clothingNumberInputs);
-  const incomeSums = getSumsFromInputs(clothingNumberInputs);
+  const foodSums = getSumsFromInputs(foodNumberInputs);
+  const livingSums = getSumsFromInputs(livingNumberInputs);
+  const transportSums = getSumsFromInputs(transportNumberInputs);
+  const entertainmentSums = getSumsFromInputs(entertainmentNumberInputs);
+  const othersSums = getSumsFromInputs(othersNumberInputs);
+  const incomeSums = getSumsFromInputs(incomeNumberInputs);
+  
+  const budgetSums = getSumsFromInputs([budgetNumberInput]);
+  
+  // if there is an error, exit function
+  if (isError) {
+    return;
+  }
 
+  // if there is no error, continue
+  const spentSums = clothingSums + foodSums + livingSums + transportSums + entertainmentSums + othersSums;
+  const remainingBudget = budgetSums - spentSums + incomeSums;
+  const surplusOrDeficit = remainingBudget > 0 ? 'Surplus' : 'Deficit';
+  output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingBudget)} ${surplusOrDeficit}</span>
+    <hr>
+    <p>Set Budget: ${budgetSums} €</p>
+    <p>Expense Sums: ${spentSums} €</p>
+    <p>Income Sums: ${incomeSums} €</p>
+  `;
+  
+  // show output because it is set as hidden in CSS.
+  output.classList.remove('hide');
 }
+
+
+
+// (optional) request from Chris: output a chart or graph that shows the spending structure in percentage
+
+
 
 // write a function to get the sums from user inputs
 function getSumsFromInputs(list) {
   let sums= 0;
+
   for (const item of list) {
     const currVal = cleanInputString(item.value);
-    const invalidInputMatch = isInvalidInput();
+    const invalidInputMatch = isInvalidInput(currVal);
+    
+    // if true that there is invalid input, prompt a pop-up message to alert the users
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    // if no invalid input is found, convert inputs into number and add it to the sums for later calculation
+    sums += Number(currVal);
   }
+  // return the sums, so that the function terminates
+  return sums;
 }
+
+// write a function to clear user inputs and outputs
+function clearForm() {
+  // select all inputs from the class input-container which contains all the user inputs
+  const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+
+  // restore user inputs to default: empty string
+  for (const container of inputContainers) {
+    container.innerHTML = '';
+  }
+  
+  // restore default values and setting
+  budgetNumberInput.value = '';
+  output.innerText = '';
+  output.classList.add('hide');
+}
+
+// add events so that the web app is interactive
+addEntryButton.addEventListener("click", addEntry);
+budgetCounter.addEventListener("submit", calculateSums);
+clearButton.addEventListener("click", clearForm);
