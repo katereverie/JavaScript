6ba87043ceb892af748a1035a3fb44ed3a3bdef8
Katereverie <zhangjt.de@gmail.com>
2024-04-08 07:35:55 +0200

    update

---
+++ curriculum-projects/Music-Player/script.js
@@ -0,0 +1,10 @@
+// access elements from HTML file
+const playlistSongs = document.getElementById('playlist-songs');
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+const shuffleButton = document.getElementById('shuffle');
+
+// create an array to store all songs
+const allSongs = [];
