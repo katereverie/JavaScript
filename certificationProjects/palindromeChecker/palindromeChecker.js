42cd5c842e209b40687ebe4256922733f01c4646
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-10 10:56:29 +0200

    update js

--- certificationProjects/palindromeChecker/palindromeChecker.js
+++ certificationProjects/palindromeChecker/palindromeChecker.js
@@ -1,9 +1,19 @@
-const textInput = document.getElementById("text-input");
-const checkButton = document.getElementById("check-btn");
-const resultInfo = document.getElementById("result");
+const textInput = document.getElementById("text-input"); // targets user's text input, this is an input el.
+const checkButton = document.getElementById("check-btn"); // targets the check button
+const resultInfo = document.getElementById("result"); // div section shows check results
 
-function cleanTextInput() {
-
+// get the input value and clean it so that factors such as punctuation, case, and spacing become irrelavent
+// store the cleaned input value into an array variable and return it as the output of the function
+function cleanTextInput(input) {
+  const cleanedInput = input.toLowerCase().replace(/[\W\s]/g, "");
+  const inputArray = cleanedInput.split("");
+  
+  const emptyInputAlert = (cleanedInput === "") => alert("Please input a value");
+  if (inputArray === inputArray.reverse()) {
+    return `${input} is a palindrome`;
+  } else {
+    return `${input} is not a palindrome`;
+  }
 }  
 
 function showResult() {
