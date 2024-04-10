d6f44c5b8dbdc4e7ba856edbad563589bf172579
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-10 10:20:53 +0200

    palindrome checker folder html css js update

---
+++ certificationProjects/palindromeChecker/palindromeChecker.js
@@ -0,0 +1,16 @@
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const resultInfo = document.getElementById("result");
+
+function cleanTextInput() {
+
+}  
+
+function showResult() {
+   
+}
+
+// regardless of forwards/backwards, punctuation, case, and spacing
+// textInput is a string. To process it, store it in a variable. the document
+
+checkButton.addEventListener("click", showResultFunction);
