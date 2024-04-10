6795f27494c52a37edfa36af1ada45575c0ae75f
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-10 15:38:41 +0200

    debugged

--- certificationProjects/palindromeChecker/palindromeChecker.js
+++ certificationProjects/palindromeChecker/palindromeChecker.js
@@ -1,14 +1,11 @@
-const textInput = document.getElementById("text-input"); // targets user's text input, this is an input el.
-const checkButton = document.getElementById("check-btn"); // targets the check button
-const resultInfo = document.getElementById("result"); // div section shows check results
+const textInput = document.getElementById("text-input"); 
+const checkButton = document.getElementById("check-btn");
+const resultInfo = document.getElementById("result"); 
 let isPalindrome = true;
 
-// get the input value and clean it so that factors such as punctuation, case, and spacing become irrelavent
-// store the cleaned input value into an array variable and return it as the output of the function
-
 const showResult = () => {
-  // clean the value of textInput element
-  const cleanedInput = textInput.value.toLocaleLowerCase().replace(/[\W\s]/g, "");
+  // clear the value of textInput element of non-word characters, underscores, and whitespace
+  const cleanedInput = textInput.value.toLocaleLowerCase().replace(/[\W\_\s]/g, "");
   // check if the cleaned input is empty
   if (cleanedInput === "") {
     alert("Please input a value");
@@ -19,11 +16,13 @@ const showResult = () => {
   // turn cleaned input into an array, reverse it and compare whether each element is equal to each other
   const arrayInput = cleanedInput.split("");
   const arrayInputReversed = arrayInput.slice().reverse();
-  for (i = 0; i < arrayInput.length; i++) {
+  
+  // don't for get "let"!!!
+  for (let i = 0; i < arrayInput.length; i++) {
     if (arrayInput[i] !== arrayInputReversed[i]) {
       isPalindrome = false;
       break;
-    } 
+    }
   }
 
   if (isPalindrome) {
@@ -37,8 +36,10 @@ const showResult = () => {
 
 const clearInput = () => {
   textInput.value = "";
+  isPalindrome = true;
 }
 
 checkButton.addEventListener("click", showResult);
+
 
 
