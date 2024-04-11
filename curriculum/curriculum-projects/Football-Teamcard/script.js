6461c693ea0c5e1a4aaf1224f416fd4362459a86
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-11 10:10:27 +0200

    update arrow func setPlayerCards

--- curriculum/curriculum-projects/Football-Teamcard/script.js
+++ curriculum/curriculum-projects/Football-Teamcard/script.js
@@ -192,4 +192,16 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 // create arrow func to display player cards based on selected options from the dropdown
-const setPlayerCards = (arr = players) => {}
+const setPlayerCards = (arr = players) => {
+  playerCards.innerHTML += arr.map(
+    ({name, position, number, isCaptain, nickname}) => 
+    `
+      <div class="player-card">
+        <h2>${name} ${isCaptain ? "(Captain)" : ""}</h2>
+        <p>Position: ${position}</p>
+        <p>Number: ${number}</p>
+        <p>Nickname: ${nickname !== null? nickname : "N/A"}</p>
+      </div>
+    `
+  ).join("");
+};
