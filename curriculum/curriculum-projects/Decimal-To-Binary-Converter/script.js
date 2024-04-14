a812553dfb57fd5e0b1ef6c2ae3f726695d25586
Katereverie <zhangjt.de@gmail.com>
2024-04-14 09:57:23 +0200

    refractor complete

--- curriculum/curriculum-projects/Decimal-To-Binary-Converter/script.js
+++ curriculum/curriculum-projects/Decimal-To-Binary-Converter/script.js
@@ -27,6 +27,16 @@ const decimalToBinary = (input) => {
 
   let binary = "";
 
+  if (input === 0) {
+    binary === "0";
+    return;
+  }
+
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
+
   result.innerText = binary;
 
 };
