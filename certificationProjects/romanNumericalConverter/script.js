fd98da40183989773fd24913cc4eb1ed233d7c30
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-15 21:04:21 +0200

    debug and update

--- certificationProjects/romanNumericalConverter/script.js
+++ certificationProjects/romanNumericalConverter/script.js
@@ -2,6 +2,8 @@ const numberInput = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("output");
 
+let r;
+
 const checkUserInput = () => {
   // convert to int
   const inputInt = parseInt(numberInput.value);
@@ -33,10 +35,115 @@ const convertInput = (input) => {
   // const countTwoDigits = [];
   // const countOneDigit = [];
 
-  while (input !== 0) {
-    
+  if (input >= 1000) {
+    const quotient = Math.floor(input / 1000);
+    const remainder = input % 1000;
+
+    while (quotient > 0) {
+      fourDigits.push("M");
+      quotient --;  
+    }
+
+    convertInput(remainder);
   } 
 
+  if (999 > input >= 100) {
+
+    const quotient = Math.floor(input / 100);
+    const remainder = input % 100;
+    
+
+    if (quotient === 9) {
+      threeDigits.push["CM"];
+    }
+
+    if (9 > quotient >= 5) {
+      threeDigits.push["D"];
+      quotient -= 5;
+
+      while (quotient > 0) {
+        threeDigits.push["C"];
+        quotient--;
+      }
+    }
+
+    if (quotient === 4) {
+      threeDigits.push("CD");
+    }
+
+    if (4 > quotient > 0) {
+
+      while (quotient !== 0) {
+        threeDigits.push("C");
+      }
+    }
+
+    convertInput(remainder);
+  }
+
+  if (99 > input >= 10) {
+
+    const quotient = Math.floor(input / 10);
+    const remainder = input % 10;
+    
+
+    if (quotient === 9) {
+      threeDigits.push["XC"];
+    }
+
+    if (9 > quotient >= 5) {
+      threeDigits.push["L"];
+      quotient -= 5;
+
+      while (quotient > 0) {
+        threeDigits.push["X"];
+        quotient--;
+      }
+    }
+
+    if (quotient === 4) {
+      threeDigits.push("XL");
+    }
+
+    if (4 > quotient > 0) {
+
+      while (quotient !== 0) {
+        threeDigits.push("X");
+      }
+    }
+
+    convertInput(remainder);
+  }
+
+  if (9 > input >= 1) {
+    
+    if (quotient === 9) {
+      threeDigits.push["IX"];
+    }
+
+    if (9 > quotient >= 5) {
+      threeDigits.push["V"];
+      quotient -= 5;
+
+      while (quotient > 0) {
+        threeDigits.push["I"];
+        quotient--;
+      }
+    }
+
+    if (quotient === 4) {
+      threeDigits.push("IV");
+    }
+
+    if (4 > quotient > 0) {
+
+      while (quotient !== 0) {
+        threeDigits.push("I");
+      }
+    }
+
+    convertInput(remainder);
+  }
   
 
   result.innerText = numberOfM.join("") + numberOfD
