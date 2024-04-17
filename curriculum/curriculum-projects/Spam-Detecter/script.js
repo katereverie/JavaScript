f844fe995950ccd86c1a22b24a93bdd7f95090fd
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-17 10:30:02 +0200

    complete, enter testing

--- curriculum/curriculum-projects/Spam-Detecter/script.js
+++ curriculum/curriculum-projects/Spam-Detecter/script.js
@@ -4,10 +4,11 @@ const checkMessageButton = document.getElementById("check-message-btn");
 
 // regex bundle
 const helpRegex = /please help/i;
-const helpOrAssistRegex = /please help|assist me/i;
+// const helpOrAssistRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thoudsand|million|billion)? dollars/i;
 const freeRegex = /(:?^|\s)fr[e3][e3] m[o0]n[e3]y(:?$|\s)/i;
 const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+const dearRegex = /(?:^|\s)d[e3][a@4]r fr[i1|][e3]nd(?:$|\s)/i;
 const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
 
 // match method 
