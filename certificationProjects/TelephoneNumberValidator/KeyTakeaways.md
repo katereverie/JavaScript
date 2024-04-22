c98846b6c254ae98878f80350293e523255ce6f0
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-22 13:02:52 +0200

    debugged no alert message

---
+++ certificationProjects/TelephoneNumberValidator/KeyTakeaways.md
@@ -0,0 +1,2 @@
+### Where to put the JS file in the HTML file?
+If you put the JS file inside the head element, the program won't run. Unless you put it inside the body element.
--- certificationProjects/TelephoneNumberValidator/tnv.js
+++ certificationProjects/TelephoneNumberValidator/tnv.js
@@ -1,11 +1,23 @@
 const checkBtn = document.getElementById("check-btn");
+const userInput = document.getElementById("user-input");
 
+// const cleanUserInput = () => {
+//   const cleanedUserInput = userInput.value;
+// }
 const checkUserInput = () => {
+  console.log(`checking user input: ${userInput.value}`);
+
+  const regex = /\s*/gi;
+
+  const cleanedUserInput = userInput.value.replace(regex, "");
+
+  console.log(`checking cleaned user input: ${cleanedUserInput}`);
   // check empty input
-  if (checkBtn.value === "") {
+  if (cleanedUserInput === "") {
     alert("Please provide a phone number");
     return;
   }
+
 }
 
 checkBtn.addEventListener("click", checkUserInput);
