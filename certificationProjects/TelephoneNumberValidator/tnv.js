6900b1a6bf86b4e5ae6657bab031985c6409d63d
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-04-22 14:47:16 +0200

    debugged check number of digits according to area code rule and country code

--- certificationProjects/TelephoneNumberValidator/tnv.js
+++ certificationProjects/TelephoneNumberValidator/tnv.js
@@ -3,22 +3,46 @@ const clearBtn = document.getElementById("clear-btn");
 const userInput = document.getElementById("user-input");
 const result = document.getElementById("results-div");
 
-// const cleanUserInput = () => {
-//   const cleanedUserInput = userInput.value;
-// }
 const checkUserInput = () => {
-  console.log(`checking user input: ${userInput.value}`);
 
-  const regex = /\s*/gi;
+  // valid symbols include: "-", "(", ")", " ". 
+  const regex = /\-*\(*\)*\s*/gi;
+
 
   const cleanedUserInput = userInput.value.replace(regex, "");
-
-  console.log(`checking cleaned user input: ${cleanedUserInput}`);
+  const isNumeric = !isNaN(cleanedUserInput);
+  console.log(`${cleanedUserInput} is considered numeric: ${isNumeric}`);
+  
   // check empty input
   if (cleanedUserInput === "") {
     alert("Please provide a phone number");
     return;
   }
+  // after cleaning the input, check whether the input is numeric
+  if (isNaN(cleanedUserInput)) {
+    return result.textContent = `Invalid US ! number: ${userInput.value}`;
+  }
+  // check the number of digits of the input number
+  // only allow 10 or 11. Below or above is invalid
+  console.log(cleanedUserInput.length);
+  if (cleanedUserInput.length < 10 || cleanedUserInput.length > 11 ) {
+    return result.textContent = `Invalid US !! number: ${userInput.value}`;
+  }
+
+  // the input has 10 or 11 digits and is numeric
+  // if the input has 11 digits, check if the first digit is 1, if it is not 1, it is invalid
+  // if the input has 10 digits, check if the first digit is within 2-9 including 2 and 9, if it is not, it is invalid
+  if (cleanedUserInput.length === 11 && parseInt(cleanedUserInput[0]) !== 1) {
+    return result.textContent = `Invalid US !!! number: ${userInput.value}`;
+  } else if (parseInt(cleanedUserInput[1]) === 1) {
+    return result.textContent = `Invalid US !!!! number: ${userInput.value}`;
+  }
+  
+  if (cleanedUserInput.length === 10 && parseInt(cleanedUserInput[0]) < 2) {
+    return result.textContent = `Invalid US !!!!! number: ${userInput.value}`;
+  }
+  
+  return result.textContent = `Valid US number: ${userInput.value}`;
 
 }
 
