427b30f69dd7b04da3c13c766bd968cd2ffa2060
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-05-05 19:57:43 +0200

    debug

--- certificationProjects/PokémonSearchApp/script.js
+++ certificationProjects/PokémonSearchApp/script.js
@@ -6,23 +6,35 @@ const spriteContainer = document.getElementById("pokemon-sprite-container");
 
 const pokedexURL = " https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
 
-let fetchedResults = [];
-
-const processData = (data) => {
-  fetchedResults = data["results"];
-}
+let fetchedResults;
+let relevantData = [];
 
 const fetchData = async () => {
   try {
     const res = await fetch(pokedexURL);
     const data = await res.json();
-    processData(data);
+    fetchedResults = data; 
+    console.log("first fetch", fetchedResults); // object 
+    fetchedResults = fetchedResults["results"];
+    console.log("assign first fetch's second property's value to it:", fetchedResults); // array
+    // destructure array
+    fetchedResults.forEach((obj) => {
+      relevantData.push(({id, name, height, weight}) => ({id, name, height, weight} = obj));
+    })
+    console.log("success?", relevantData);
   } catch (err) {
     console.log("There is an error:", err);
   }
 }
 
 fetchData();
+
+const processData = async (data) => {
+  fetchedResults = data;
+  console.log(Array.isArray(fetchedResults));
+  relevantData = fetchedResults.map(({ id, name, height, weight, sprites, stats}) => ({ id, name, height, weight, sprites, stats}));
+  console.log("Success?", relevantData);
+}
 
 const processInput = () => {
   console.log("Step 2");
@@ -58,13 +70,22 @@ const hasMatch = (processedInput) => {
 }
 
 const displaySearchResults = (matchedInput, id) => {
+  console.log("step 4");
+  // const getUrl = fetchedResults[id-1]["url"];
+  console.log("url data:", extractedIdUrl);
 
   if (matchedInput === id) {
+    const toDisplayName = fetchedResults[id-1]["name"].slice();
     console.log("User has input an id:", id, matchedInput);
-    pokemonName.innerHTML += `<span id="${id}">${fetchedResults[id-1]["name"].charAt(0).toUpperCase() }</span>`;
+    pokemonName.innerHTML = `<span>Name: ${toDisplayName.charAt(0).toUpperCase() + toDisplayName.slice(1)}</span>`;
+    pokemonId.innerHTML = `<span>Pokédex-ID: ${id}</span>`;
+
   } else {
     console.log(`user has input ${matchedInput}, its ID is: ${id}`)
+    pokemonName.innerHTML = `<span>Name: ${matchedInput.charAt(0).toUpperCase() + matchedInput.slice(1)}</span>`;
+    pokemonId.innerHTML = `<span>Pokédex-ID: ${id}</span>`;
   }
+
 
 }
 
