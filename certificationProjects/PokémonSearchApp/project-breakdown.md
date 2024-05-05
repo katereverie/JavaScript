f17a2f718885fc5d3373684dd2827d49af3dd49c
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-05-05 18:13:49 +0200

    add fetchData async func; hasMatch func, processInput func, processData func

--- certificationProjects/PokémonSearchApp/project-breakdown.md
+++ certificationProjects/PokémonSearchApp/project-breakdown.md
@@ -4,3 +4,24 @@ Use case & Third Party API
 This application will search for Pokémon by name or ID, and displays the results to the user.
 This application utilizes freeCodeCamp's PokéAPI Proxy for retrieving Pokémon data and images.
 
+## Get DOM & URLs
+
+1. Prepare DOM.
+2. Prepare URLs.
+
+## Request & Parse Data
+
+Analysis of Source Data
+
+- The source data consists of one object with two pairs: `count: 1302` and `results: [...]`.
+- The value of `results` is an array with 1302 elements.
+- Log `results.[0]` in the console, we see that each array element is an object.
+- Each object consists of three pairs of property/value.
+- 1st pair: `id: 1`; 2nd pair: `name: "bulbasaur"`; 3rd pair: `url: "..."`
+- Get this *id*, *name*, and *url* because I need to display it later.
+- Click on the `url`, we see the data we need to display on the Pokédex.
+  - Specifically: 
+Functions
+
+- Create `const showSearchResults = (data) => {}`
+- Inside the body, deconstruct `id`, `name`, and `url`.
--- certificationProjects/PokémonSearchApp/script.js
+++ certificationProjects/PokémonSearchApp/script.js
@@ -1,1 +1,74 @@
+const userInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-button");
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const spriteContainer = document.getElementById("pokemon-sprite-container");
+
+const pokedexURL = " https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+
+let fetchedResults = [];
+
+const processData = (data) => {
+  fetchedResults = data["results"];
+}
+
+const fetchData = async () => {
+  try {
+    const res = await fetch(pokedexURL);
+    const data = await res.json();
+    processData(data);
+  } catch (err) {
+    console.log("There is an error:", err);
+  }
+}
+
+fetchData();
+
+const processInput = () => {
+  console.log("Step 2");
+  if (isNaN(userInput.value)) {
+    const nameInput = userInput.value.toLowerCase();
+    console.log("user has input a name:", nameInput);
+    hasMatch(nameInput);
+  } else {
+    const idInput = parseInt(userInput.value);
+    console.log("user has input an id:", idInput);
+    hasMatch(idInput);
+  }
+}
+
+const hasMatch = (processedInput) => {
+  console.log("Step 3");
+  let indexCount = 0;
+  const foundMatch = fetchedResults.some(obj => {
+    const valuesArray = Object.values(obj);
+    indexCount++;
+    console.log(indexCount, valuesArray);
+    return valuesArray.includes(processedInput);
+  })
+
+  console.log("This is the ID of the matched Pokemon:", indexCount);
+
+  if (foundMatch) {
+    console.log("We found a matching! Displaying ...");
+    displaySearchResults(processedInput, indexCount);
+  } else {
+    alert("Pokémon not found");
+  }
+}
+
+const displaySearchResults = (matchedInput, id) => {
+
+  if (matchedInput === id) {
+    console.log("User has input an id:", id, matchedInput);
+    pokemonName.innerHTML += `<span id="${id}">${fetchedResults[id-1]["name"].charAt(0).toUpperCase() }</span>`;
+  } else {
+    console.log(`user has input ${matchedInput}, its ID is: ${id}`)
+  }
+
+}
+
+searchBtn.addEventListener("click", () => {
+  console.log("Step 1");
+  processInput();
+});
