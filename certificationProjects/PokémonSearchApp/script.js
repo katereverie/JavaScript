6795e45863d3ce3fe253735c37eb7018e53efff4
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-05-06 14:21:22 +0200

    cleanup code

--- certificationProjects/PokémonSearchApp/script.js
+++ certificationProjects/PokémonSearchApp/script.js
@@ -14,8 +14,6 @@ const pokemonSpDefense = document.getElementById("special-defense");
 const pokemonSpeed = document.getElementById("speed");
 const pokemonStatsTotal = document.getElementById("total-stats");
 
-const spriteContainer = document.getElementById("pokemon-sprite-container");
-
 // Use this get id, name, and URL of other data
 const pokemonList = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
 // concatenate the id or name to this URL to get other data
@@ -25,33 +23,12 @@ let fetchedList;
 let relevantData = [];
 let statsTotal = 0;
 
-const testObjInArr = [
-  {
-    id: 1,
-    name: "AsianBoss",
-    stringUrl: "Fake"
-  },
-  {
-    id: 2,
-    name: "EuropeanBoss",
-    stringUrl: "True"
-  }
-]
-
-console.log("testing get the first element of array:", testObjInArr[0]);
-
 const fetchList = async () => {
-  console.log("fetching list");
   try {
     const res = await fetch(pokemonList);
     const data = await res.json();
     fetchedList = data; 
-    console.log("first fetch", fetchedList); // object 
     fetchedList = fetchedList["results"];
-    console.log("assign first fetch's second property's value to it:", fetchedList); // array
-    console.log("Fetched List is an array?", Array.isArray(fetchedList));
-    console.log(`Its length is ${fetchedList.length}`);
-    console.log("Its first element is an objects:", fetchedList[0]);
   } catch (err) {
     console.log("There is an error:", err);
   }
@@ -60,12 +37,10 @@ const fetchList = async () => {
 fetchList();
 
 const fetchData = async (id) => {
-  console.log("fetching data");
   try {
     const res = await fetch(`${pokemonData + id}`);
     const data = await res.json();
     const relevantData = data;
-    console.log("checking if found the correct relevant data:", relevantData);
     displayRelevantData(relevantData);
   } catch (err) {
     console.log("There is an error fetching relevant data:", err);
@@ -73,30 +48,24 @@ const fetchData = async (id) => {
 }
 
 const processInput = () => {
-  console.log("processing input");
   if (isNaN(userInput.value)) {
     const nameInput = userInput.value.toLowerCase();
-    console.log(`User has input a name: ${nameInput}`);
     hasMatch(nameInput);
   } else {
     const idInput = parseInt(userInput.value);
-    console.log(`user has input an id: ${idInput}`);
     hasMatch(idInput);
   }
 }
 
 const hasMatch = (processedInput) => {
-  console.log("now searching for a match...");
   let indexCount = 0;
   const foundMatch = fetchedList.some(obj => {
     const valuesArray = Object.values(obj);
     indexCount++;
-    console.log(indexCount, valuesArray);
     return valuesArray.includes(processedInput);
   })
 
   if (foundMatch) {
-    console.log("Matched found! This is the ID of the matched Pokemon:", indexCount);
     displayIdName(processedInput, indexCount);
   } else {
     alert("Pokémon not found");
@@ -104,12 +73,10 @@ const hasMatch = (processedInput) => {
 }
 
 const displayIdName = (matchedInput, id) => {
-  console.log("Displaying data of matched Pokemon...");
   const toDisplayName = fetchedList[id-1]["name"].slice();
 
 
   if (matchedInput === id) {
-    console.log("User has input an id:", id, matchedInput);
     pokemonName.innerHTML = `<span>${toDisplayName.toUpperCase()}</span>`;
     pokemonId.innerHTML = `<span>${id}</span>`;
 
@@ -146,7 +113,14 @@ const displayRelevantData = (relevantData) => {
 }
 
 searchBtn.addEventListener("click", () => {
-  console.log("user clicked search");
   pokemonTypes.innerHTML = "";
   processInput();
+});
+
+userInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    pokemonTypes.innerHTML = "";
+    processInput();
+  }
 });
