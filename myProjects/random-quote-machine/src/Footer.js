56835b46230f577ab23460e50fdc9ef4a581452a
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-05-24 16:30:39 +0200

    detele

--- myProjects/random-quote-machine/src/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} <span id="app-author">Katereverie</span></p>
-    </footer>
-  );
-}
-
-export default Footer
